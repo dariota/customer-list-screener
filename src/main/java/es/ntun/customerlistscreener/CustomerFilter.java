35691ae92b106c79f9086671cb8f291725f48fc4
Dário Tavares Antunes <dario@ntun.es>
2018-02-16 10:28:32 +0000

    Implement customer filtering

--- src/main/java/es/ntun/customerlistscreener/CustomerFilter.java
+++ src/main/java/es/ntun/customerlistscreener/CustomerFilter.java
@@ -7,9 +7,22 @@ import es.ntun.customerlistscreener.customer.EarthPoint;
 
 public class CustomerFilter {
 
+	// could be a stream, but that's not great for efficiency
 	public static ArrayList<Customer> findCustomersWithinOf(Iterable<Customer> customers,
 	                                                        double distanceMetres,
 	                                                        EarthPoint location) {
-		return null;
+		ArrayList<Customer> filtered = new ArrayList<>();
+
+		if (customers == null || location == null) {
+			return filtered;
+		}
+
+		for (Customer c : customers) {
+			if (location.distanceTo(c.getLocation()) <= distanceMetres) {
+				filtered.add(c);
+			}
+		}
+
+		return filtered;
 	}
 }
